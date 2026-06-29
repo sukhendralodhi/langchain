c8bf70a50c294e355acdf1e297765422167c468c
Sukhendra <developer.sukhendra@gmail.com>
2026-06-29 23:31:47 +0530

    ollama

---
+++ embeding/ollama.js
@@ -0,0 +1,14 @@
+import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import "dotenv/config";
+
+const embeddings = new GoogleGenerativeAIEmbeddings({
+    model: "gemini-embedding-001",
+    googleApiKey: process.env.GOOGLE_API_KEY,
+});
+
+const vector = await embeddings.embedQuery(
+    "What is React?"
+);
+
+console.log(vector.length);
+// console.log(vector.slice(0, 10));
